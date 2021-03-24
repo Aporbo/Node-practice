be07b389bda33ed5b71d23b8d975fc7c7f1be687
Aporbo Ghosh <61919274+Aporbo@users.noreply.github.com>
2021-03-25 05:19:34 +0600

    added client site

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express')
-
-
+const cors = require('cors')
+const bodyParser = require('body-parser');
 const app = express();
-
+app.use(cors())
+app.use(bodyParser.json());
+const users= ["asad",'aporbo','ayesha','hridy','sakib','sabana']
 
 app.get('/', (req,res)=>{
     res.send('thank you very much');
 }
 )
+
+app.get('/fruits/banana', (req,res)=>{
+    res.send({fruits:'banana',quantity:1000,price:1000});
+}
+)
+
+app.get('/users/:id',(req,res)=>{
+    const id = req.params.id
+    const name = users[id];
+    res.send({id,name});
+})
+
+app.post('/adduser', (req,res)=>{
+    const user = req.body;
+    user.id =55;
+   res.send(user)
+})
 
 
 
